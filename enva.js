2cb2f5eb3052af160f83b7ddbcbbe0605a60d63b
AndiXplorer <andi_cakolli@outlook.com>
2025-01-27 17:34:13 +0100

    Testing

---
+++ enva.js
@@ -0,0 +1,11 @@
+var a = 5;
+var b = 5;
+var operatori = "-"
+
+if(operatori == "+"){
+    console.log(a + b);
+} else if(operatori == "-"){
+    console.log(a - b);
+} else if(operatori == "*"){
+    console.log(a * b);
+}
